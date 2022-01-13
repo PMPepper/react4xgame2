4996c0cb7b870eb0186828912a2e30da618a30a9
DESKTOP-2D91HMA\Paul <pm_pepper@hotmail.com>
2022-01-13 22:48:17 +0000

    WIP added totally untested code for context based selectors

---
+++ src/components/SelectableContext/index.js
@@ -0,0 +1,91 @@
+//TODO test to find out if any of this works
+import { createContext, useContext, useRef, useEffect } from "react"
+
+const defaultContext = createContext(null);
+defaultContext.displayName = 'SelectableContext';
+
+
+export default function Provider({children, value, context = defaultContext}) {
+    const ref = useRef(null);
+
+    if(ref.current === null) {
+        ref.current = new ContextState(value);
+    }
+
+    useEffect(
+        () => {
+            ref.current.setState(value);
+        },
+        [value]
+    );
+
+    return <context.Provider value={ref.current}>
+        {children}
+    </context.Provider>
+}
+
+
+
+class ContextState {
+    constructor(initialValue) {
+        this.state = initialValue;
+
+        this.subscribers = [];
+    }
+
+    getState() {
+        return this.state;
+    }
+
+
+    setState(newState) {
+        if(newState !== this.state) {
+            this.state = newState;
+
+            //execute all subscriber callbacks with the new value of the state
+            this.subscribers.forEach(subscriber => subscriber(newState));
+        }
+    }
+
+    subscribe(callback) {
+        subscribers.push(callback);
+
+        //returns unsubscribe method
+        return () => {
+            const index = this.subscribers.indexOf(callback);
+
+            if(index !== -1) {
+                this.subscribers.splice(index, 1);
+            }
+        }
+    }
+}
+
+//Hooks
+const refEquality = (a, b) => a === b;
+
+export function useContextSelector(selector, equalityFn = refEquality, context = defaultContext) {
+    const state = useContext(context);
+
+    if(!state || !(state instanceof ContextState)) {
+        throw new Error('No valid SelectableContext found')
+    }
+
+    const selectedState = useSubscribeToDataSource(
+        store.subscribe,
+        store.getState,
+        selector,
+        equalityFn,
+    )
+
+    return selectedState;
+}
+
+export function useGetContextState(context = defaultContext) {
+    const state = useContext(context);
+
+    return state?.value;
+}
+
+
+
