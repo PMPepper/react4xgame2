4ce3beaebaa0d66c859e714cf6b4cd30df12eb81
DESKTOP-2D91HMA\Paul <pm_pepper@hotmail.com>
2022-06-05 14:32:30 +0100

    Menu WIP (getting there...)

--- src/components/ui/Menu/index.js
+++ src/components/ui/Menu/index.js
@@ -1,5 +1,5 @@
-import { useRef, forwardRef, useCallback, useEffect, useState, useMemo, useContext } from "react";
-import PropTypes from "prop-types";
+import { useRef, forwardRef, useCallback, useEffect, useMemo } from "react";
+//import PropTypes from "prop-types";
 import mergeRefs from "react-merge-refs";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
@@ -10,175 +10,56 @@ import MenuDisplay from "components/display/Menu";
 
 //Hooks
 import useElementPosition from "hooks/useElementPosition";
-import useRefCallback from "hooks/useRefCallback";
-
-//Helpers
-import combineProps from "helpers/react/combine-props";
+import useId from "hooks/useId";
 
 //Other
-import classes from './Menu.module.scss';
-
-import { e } from "mathjs";
-import { getNextKeyDef } from "@testing-library/user-event/dist/keyboard/getNextKeyDef";
-
-//Consts
-// const MenuContext = createContext();
-// MenuContext.displayName = 'MenuContext';
+import useMenuState from './reducer';
 
 
 //The component
-const Menu = forwardRef(function ({items, ...rest}, ref) {
-    //state
+const Menu = forwardRef(function ({items, id, ...rest}, ref) {
+    id = useId(id, 'menu');
+
+    const [{itemsSelectedAtLevel, itemsOpenAtLevel}, dispatch] = useMenuState(items);
+
+    useEffect(//keep focus updated based on selected item
+        () => {
+            const {current: rootElem} = rootElemRef
+
+            if(itemsSelectedAtLevel.length > 0) {
+                document.getElementById(getItemId(id, ...itemsSelectedAtLevel))?.focus();
+            } else {
+                console.log('focus on root');
+                rootElem?.focus();
+            }
+        },
+        [itemsSelectedAtLevel]
+    )
+
+    //other state
     const rootElemRef = useRef();
-    const [level, setLevel] = useState(0);
-    const [selected, setSelected] = useState([]);
-    const [openAtLevel, setOpenAtLevel] = useState([]);
 
-    //callbacks
-    const selectNext = useRefCallback(() => {
-        const curSelected = selected[level];
-
-        console.log('selectNext, ', curSelected, level);
-
-        if((curSelected ?? false) === false) {
-            selectFirst();
-        } else {
-            const levelItems = getLevelItems(items, selected, level);
-
-            const newSelectedIndex = levelItems.findIndex((item, index) => {
-                console.log(item, index);
-                return (index > curSelected) && (item !== Menu.DividerName);
-            })
-            console.log(newSelectedIndex);
-            if(newSelectedIndex === -1) {
-                selectFirst()
-            } else {
-                setSelectedItem(newSelectedIndex, level);
-            }
-        }
-    });
-
-    const selectPrev = useRefCallback(() => {
-        const curSelected = selected[level];
-
-        console.log('selectPrev, ', curSelected, level);
-
-        if((curSelected ?? false) === false) {
-            selectLast();
-        } else {
-            const levelItems = getLevelItems(items, selected, level);
-
-            const newSelectedIndex = levelItems.findLastIndex((item, index) => {
-                console.log(item, index);
-                return (index < curSelected) && (item !== Menu.DividerName);
-            })
-
-            console.log(newSelectedIndex);
-            if(newSelectedIndex === -1) {
-                selectLast()
-            } else {
-                setSelectedItem(newSelectedIndex, level);
-            }
-        }        
-    });
-
-    const selectFirst = useRefCallback((atLevel = null) => {
-        atLevel = atLevel ?? level;
-        console.log('selectFirst: ', atLevel, selected);
-        const levelItems = getLevelItems(items, selected, atLevel);
-
-        const newSelectedIndex = levelItems.findIndex((item) => {
-            return item !== Menu.DividerName;
-        })
-
-        if(newSelectedIndex !== -1) {
-            setSelectedItem(newSelectedIndex, atLevel);
-        } else {
-            setSelectedItem(null, atLevel);
-        }
-    });
-
-    const selectLast = useRefCallback(() => {
-        console.log('selectLast');
-
-        const levelItems = getLevelItems(items, selected, level);
-
-        const newSelectedIndex = levelItems.findLastIndex((item, index) => {
-            return item !== Menu.DividerName;
-        })
-
-        if(newSelectedIndex !== -1) {
-            setSelectedItem(newSelectedIndex, level);
-        } else {
-            setSelectedItem(null, level);
-        }
-    });
-
-    const openSubmenu = useRefCallback(() => {
-        const item = getCurrentItem(items, selected);
-
-        if(item?.items?.length > 0) {
-            setOpenItem(selected[level], level);
-        }
-    });
-
-    const closeSubmenu = useRefCallback(() => {
-        setOpenAtLevel(openAtLevel => openAtLevel.slice(0, -1));
-    });
-
-    const onKeyDown = useRefCallback(
+    const onKeyDown = useCallback(
         (e) => {
-            console.log('Menu::onKeyDown('+e.which+')');
-            //tab key is ignored
-            //esc key closes
-
-            //if no items selected:
-                //if enter key, down or right pressed, select first item
-                //if up key pressed, select last item
-            //else
-                //down key selected next item
-                //up key selects previous item
-                //left key goes up a level (if applicable)
-                //right key goes down a level (if applicable)
-                //enter key clicks item
-            
             switch(e.which) {
                 case 9://Tab
-                    break;//TODO ignore
+                    break;//ignore
                 case 13://Enter
-                    break;//TODO click item
-                case 40:
-                    selectNext();
+                    document.activeElement.click();//click item
                     break;
-                case 38:
-                    selectPrev();
+                case 40://Down
+                    dispatch({type: 'keyboardDown'});
+                    break;
+                case 38://Up
+                    dispatch({type: 'keyboardUp'});
                     break;
                 case 39: {//right
-                    const item = getCurrentItem(items, selected);
-
-                    if(item?.items?.length > 0) {
-                        const newLevel = level + 1;
-                        openSubmenu();
-                        setLevel(newLevel);
-                        selectFirst(newLevel);
-                    }
-                    
-                    break;//TODO
+                    dispatch({type: 'keyboardRight'});
+                    break;
                 }
                 case 37: {//left
-                    const deepestOpenLevel = openAtLevel.length;
-
-                    if(deepestOpenLevel > 0) {
-                        const newLevel = deepestOpenLevel - 1;
-
-                        closeSubmenu();
-                        setLevel(newLevel);
-
-                        //set selected to parent of current open item
-                        setSelected([...openAtLevel]);
-                    }
-                    
-                    break;//TODO
+                    dispatch({type: 'keyboardLeft'});
+                    break;
                 }
                 default:
                     return;
@@ -190,62 +71,57 @@ const Menu = forwardRef(function ({items, ...rest}, ref) {
         }
     );
 
-    const setSelectedItem = useCallback(
-        (index, level) => {
-            console.log('setSelectedItem ', index, level);
-            if(index === null) {
-                setSelected(curSelected => (curSelected.slice(0, level)));
-            } else {
-                setSelected(curSelected => ([...curSelected.slice(0, level), index]));
-            }
-        },
-        []
-    );
-
-    const setOpenItem = useCallback(
-        (index, level) => {
-            console.log('setSelectedOpen ', index, level);
-            setOpenAtLevel(current => ([...current.slice(0, level), index]))
-        },
-        []
-    );
-
     const setRootElem = useCallback(
         (elem) => {
             //need to delay setting focus for some reason
-            elem && setTimeout(() => {elem.focus()}, 0);
+            elem && setTimeout(() => {
+                elem.focus();
+            }, 0);
             rootElemRef.current = elem;
         },
         []
     );
 
     const onMouseEnter = useCallback(
-        (index, level) => {
-            setSelectedItem(index, level);
-            //is this going to work?
-            openSubmenu();
+        (e) => {
+            const dataset = e.currentTarget.dataset;
+
+            dispatch({type: 'mouseEnter', payload: {index: +dataset.menuIndex, level: +dataset.menuLevel}});
         },
+        []
+    );
+
+    const onMouseLeave = useCallback(
+        (e) => {
+            const dataset = e.currentTarget.dataset;
+
+            dispatch({type: 'mouseLeave', payload: {index: +dataset.menuIndex, level: +dataset.menuLevel}});
+        },
+        []
+    );
+
+    const itemProps = useMemo(
+        () => ({
+            onMouseEnter,
+            onMouseLeave,
+            tabIndex: '0',
+        }),
         []
     );
 
     const menuProps = useMemo(
         () => ({
             onKeyDown, 
+            onMouseLeave: (e) => dispatch({type: 'mouseLeaveMenu'}),
+            onContextMenu: (e) => {e.preventDefault(); e.stopPropagation();},
             tabIndex: 0,
+            id,
             ref: mergeRefs([ref, setRootElem])
         }),
         []
     );
 
-    // const content = useMemo(
-    //     () => ({
-    //         setSelectedId, 
-    //         selectedId
-    //     }),
-    //     [items]
-    // );
-
-    return renderMenu(items, 0, level, selected, openAtLevel, menuProps, onMouseEnter);
+    return renderMenu(items, [], 0, itemsSelectedAtLevel, itemsOpenAtLevel, menuProps, id, itemProps, dispatch);
 });
 
 Menu.displayName = 'Menu';
@@ -253,38 +129,13 @@ Menu.displayName = 'Menu';
 export default Menu;
 
 //Internal helpers
-function getItemAt(items, path, level = 0) {
-    const index = path[level];
-
-    if(path.length === level + 1) {
-        return items[index];
-    }
-
-    return getItemAt(items[index].items, path, level + 1);
+function getItemId(rootId, ...path) {
+    return `${rootId}${path.map(i => `[${i}]`).join('')}`;
 }
 
-function getCurrentItem(items, selected, curLevel = 0) {
-    if(items.length === 0) {
-        return null;
-    }
-
-    if(selected.length -1 === curLevel) {
-        return items[selected[curLevel]]
-    }
-
-    return getCurrentItem(items[selected[curLevel]].items, selected, curLevel + 1)
-}
-
-function getLevelItems(items, selected, level, curLevel = 0) {
-    if(level === curLevel) {
-        return items
-    }
-
-    return getLevelItems(items[selected[curLevel]].items, selected, level, curLevel+1);
-}
-
-function renderMenu(items, level, selectedLevel, selected, openAtLevel, menuProps, onMouseEnter) {
-    const itemSelectedAtThisLevel = selected[level];
+function renderMenu(items, path, selectedLevel, itemsSelectedAtLevel, itemsOpenAtLevel, menuProps, rootId, itemProps, dispatch) {
+    const level = path.length;
+    const itemSelectedAtThisLevel = itemsSelectedAtLevel[level];
 
     return <MenuDisplay {...menuProps}>
         {items.map((item, i) => {
@@ -294,26 +145,44 @@ function renderMenu(items, level, selectedLevel, selected, openAtLevel, menuProp
 
             const isSelected = itemSelectedAtThisLevel === i;
             const {label, info, icon, items, onClick} = item;
+            const hasChildren = items?.length > 0;
 
             return <MenuDisplay.Item
+                id={getItemId(rootId, ...path, i)}
                 selected={isSelected}
                 key={i}
                 icon={icon}
                 children={label}
                 info={info}
-                subMenu={items && items.length > 0 ? //TODO separate selected from open
+                subMenu={items && items.length > 0 ?
                     <>
                         <FontAwesomeIcon icon={solid('caret-right')} />
-                        {(openAtLevel[level] === i) && <SubMenuWrapper>
-                            {renderMenu(items, level+1, selectedLevel, selected, openAtLevel, null, onMouseEnter)}
+                        {(itemsOpenAtLevel[level] === i) && <SubMenuWrapper>
+                            {renderMenu(items, [...path, i], selectedLevel, itemsSelectedAtLevel, itemsOpenAtLevel, null, rootId, itemProps, dispatch)}
                         </SubMenuWrapper>}
                     </>
                     :
                     null
                 }
 
-                onClick={(!items && onClick) || null}
-                onMouseEnter={() => onMouseEnter(i, level)}
+                role={!hasChildren && onClick ? "button" : undefined}
+                onClick={hasChildren ?
+                    (e) => {
+                        const dataset = e.currentTarget.dataset;
+
+                        dispatch({type: 'openMenu', payload: {index: +dataset.menuIndex, level: +dataset.menuLevel}});
+
+                        e.preventDefault();
+                        e.stopPropagation();
+                     }
+                     :
+                     onClick
+                }
+
+                data-menu-index={i}
+                data-menu-level={level}
+
+                {...itemProps}
             />
         })}
 
@@ -328,48 +197,7 @@ Menu.propTypes = {
     
 };
 
-Menu.DividerName = 'divider';
-
-// const Item = forwardRef(function Item({children, icon, info, subMenu, rest}, ref) {
-//     const [self, setSelf] = useState();
-//     const {selectedId, setSelectedId} = useContext(MenuContext);
-//     const id = useId(rest.id, 'menuItem');
-
-//     const isSelected = selectedId === id;
-
-//     const props = useMemo(
-//         () => {
-//             const calculated = {
-//                 className: classNames(classes.menuItem, isSelected && classes.selected),
-//                 tabIndex: rest.onClick ? "0" : undefined,
-//                 ref: mergeRefs([ref, setSelf]),
-//                 id,
-
-//                 onMouseEnter: () => {
-//                     setSelectedId(id)
-//                 },
-
-//             };
-
-//             return combineProps(calculated, rest);
-//         },
-//         [rest, ref, setSelf, id, setSelectedId, isSelected]
-//     );
-
-//     return <div {...props}>
-//         <span className={classes.icon}>{icon}</span>
-//         <span className={classes.content}>{children}</span>
-//         <span className={classes.info}>{info}</span>
-//         <span className={classes.subMenu}>
-//             {subMenu ? <>
-//                 <FontAwesomeIcon icon={solid('caret-right')} />
-//                 <SubMenuWrapper parent={self}>{subMenu}</SubMenuWrapper>
-//             </>
-//             :
-//             null}
-//         </span>
-//     </div>
-// });
+export const DividerName = Menu.DividerName = 'divider';
 
 const elementSizeOptions = {width: true, height: true, x: true, y: true, getElement: (elem) => elem ? elem.parentElement.parentElement : null};
 
---
+++ src/components/ui/Menu/reducer.js
@@ -0,0 +1,240 @@
+import { useReducer, useEffect, useRef } from "react";
+import {DividerName} from './index';
+
+
+export default function useMenu(items) {
+    const ref = useRef();
+
+    if(!ref.current) {
+        ref.current = {
+            firstTime: true
+        };
+    }
+
+    const [state, dispatch] = useReducer(reducer, initialState, (initialState) => ({
+        ...initialState,
+        items: normaliseItemsForState(items)
+    }));
+
+    //keeps state updated with items
+    useEffect(
+        () => {
+            if(ref.current.firstTime) {//skip the first time
+                ref.current.firstTime = false;
+            } else {
+                dispatch({type: 'updateItems', payload: normaliseItemsForState(items)});
+            }
+        },
+        [items]
+    );
+
+    
+    return [state, dispatch]
+}
+
+//recursiverly convert items into simple nested array - dividers = null, items with no children = [], and items with children come an array of their children
+function normaliseItemsForState(items) {
+    return items.map(item => {
+        if(item === DividerName) {
+            return null
+        } else if(!item?.items?.length) {
+            return []
+        } else {
+            return normaliseItemsForState(item.items);
+        }
+    })
+}
+
+
+export const initialState = {
+    items: null,
+    itemsSelectedAtLevel: [],
+    itemsOpenAtLevel: []
+};
+
+
+function reducer(state, {type, payload}) {
+    console.log('[MSR] ', type, payload);
+    switch(type) {
+        //General events
+        case 'updateItems':
+            return {
+                ...state,
+                items: payload
+            }
+        //Mouse events
+        case 'mouseEnter':{
+            //TODO clear any existing open menu timers
+            //TODO if selected item has children,
+            //-set timer to open it's submenu (this will close other open menus)
+            //-ELSE set timer to close other submenus
+
+
+            //if any other sub-menus open, set timer to close them (what about cancelling them?)
+
+
+            //TODO set timer to close lower level menus
+            //TODO clear above timers if applicable
+            const newState = setItemsSelectedAtLevel(state, payload.level, payload.index);
+
+            const item = getSelectedItem(newState);
+
+
+
+            if(item?.length > 0) {
+                //TODO slight delay before opening item
+                //return setItemsOpenAtLevel(newState, payload.level, payload.index)
+            }
+
+            return newState;
+        }
+        case 'mouseLeave': {
+
+
+            return state;
+        }
+        case 'mouseLeaveMenu': {
+            //deselect the deepest open level
+            const {itemsOpenAtLevel} = state;
+            
+            return setItemsSelectedAtLevel(state, itemsOpenAtLevel.length, null)
+        }
+        case 'openMenu': {
+            return setItemsOpenAtLevel(state, payload.level, payload.index);
+        }
+
+        //keyboard events
+        case 'keyboardDown':{
+            const {itemsSelectedAtLevel} = state;
+            const items = getItemsAtSelectedLevel(state) || state.items;
+            const level = itemsSelectedAtLevel.length ? itemsSelectedAtLevel.length -1 : 0;
+            const index = getNextSelectableItemIndex(items, itemsSelectedAtLevel[level] ?? -1)
+
+            return setItemsSelectedAtLevel(state, level, index);
+        }
+        case 'keyboardUp':{
+            const {itemsSelectedAtLevel} = state;
+            const items = getItemsAtSelectedLevel(state) || state.items;
+            const level = itemsSelectedAtLevel.length ? itemsSelectedAtLevel.length -1 : 0;
+            const index = getPrevSelectableItemIndex(items, itemsSelectedAtLevel[level] ?? items.length)
+
+            return setItemsSelectedAtLevel(state, level, index);
+        }
+        case 'keyboardLeft':{
+            const {itemsSelectedAtLevel, itemsOpenAtLevel} = state;
+
+            if(itemsSelectedAtLevel.length > 1) {//there is a level to go up
+                //close current topmost menu
+                //select parent item to the topmost menu
+                const level = itemsSelectedAtLevel.length - 2;
+                
+                return setItemsSelectedAtLevel(
+                    setItemsOpenAtLevel(state, level, null),
+                    level,
+                    itemsOpenAtLevel[level]
+                );
+            }
+
+            return state;
+        }
+        case 'keyboardRight':{
+            const {itemsSelectedAtLevel, items} = state;
+
+            if(itemsSelectedAtLevel.length === 0) {//no items selected, select first item at root level
+                return setItemsSelectedAtLevel(state, 0, getNextSelectableItemIndex(items, -1));
+            }
+
+            const item = getSelectedItem(state);
+
+            if(item?.length) {
+                const level = itemsSelectedAtLevel.length - 1;
+
+                return setItemsSelectedAtLevel(
+                    setItemsOpenAtLevel(state, level, itemsSelectedAtLevel[level]),
+                    level + 1,
+                    getNextSelectableItemIndex(item, -1)//find the first selectable child
+                );
+            }
+            
+            return state;
+        }
+        //unknown type
+        default:
+            console.log('Unknown type')
+            return state;
+    }
+}
+
+function getSelectedItem({items, itemsSelectedAtLevel}) {
+    if(itemsSelectedAtLevel.length === 0) {
+        return null;//nothing is selected
+    }
+
+    for(let i = 0; i < itemsSelectedAtLevel.length; i++) {
+        items = items[itemsSelectedAtLevel[i]];
+    }
+
+    return items;
+}
+
+function getNextSelectableItemIndex(items, startIndex) {
+    const nextIndex = items.findIndex((item, index) => {
+        return !!item && index > startIndex
+    })
+
+    return nextIndex === -1 ?
+        items.findIndex((item) => {
+            return !!item
+        })
+        :
+        nextIndex;
+}
+
+function getPrevSelectableItemIndex(items, startIndex) {
+    const prevIndex = items.findLastIndex((item, index) => {
+        return !!item && index < startIndex
+    })
+
+    return prevIndex === -1 ?
+        items.findLastIndex((item) => {
+            return !!item
+        })
+        :
+        prevIndex;
+}
+
+function getItemsAtSelectedLevel({items, itemsSelectedAtLevel}) {
+    if(itemsSelectedAtLevel.length === 0) {
+        return null;//nothing is selected
+    }
+
+    for(let i = 0; i < itemsSelectedAtLevel.length -1; i++) {
+        items = items[itemsSelectedAtLevel[i]];
+    }
+
+    return items;
+}
+
+function setItemsSelectedAtLevel(state, level, index) {
+    const {itemsSelectedAtLevel} = state;
+
+    return {
+        ...state,
+        itemsSelectedAtLevel: index === null ?
+            itemsSelectedAtLevel.slice(0, level)
+            :
+            [...itemsSelectedAtLevel.slice(0, level), index]
+    }
+}
+
+function setItemsOpenAtLevel(state, level, index) {
+    const {itemsOpenAtLevel} = state;
+
+    return {
+        ...state,
+        itemsOpenAtLevel: index === null ?
+        itemsOpenAtLevel.slice(0, level)
+            :
+            [...itemsOpenAtLevel.slice(0, level), index]
+    }
+}
